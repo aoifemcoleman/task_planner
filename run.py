8085837679bd397b96e2b3d5e2340ba0ced392a1
Aoife Coleman <aoifemcoleman@hotmail.com>
2024-02-07 20:32:43 +0000

    Create first function to show task planner menu and tasks in columns

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+def show_menu():
+    menu_items = ['1. View task planner', '2. Add task to planner', '3. Mark task as complete', '4. Remove task', '5. Exit task planner']
+    print('What would you like to do today?')
+    for item in menu_items:
+        print(item)
+
+show_menu()
