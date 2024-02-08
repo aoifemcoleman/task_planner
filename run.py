70a305b87c5075b089ea3bf811ea75bff1a9ede3
Aoife Coleman <aoifemcoleman@hotmail.com>
2024-02-08 14:39:35 +0000

    Create list for tasks within main function, and pass tasks as paramter to functions within menu_choice function

--- run.py
+++ run.py
@@ -11,23 +11,25 @@ def show_menu():
 def menu_choice():
     choice = input('Please enter a number between 1-5, corresponding to your choice. ')
     if choice == "1":
-            view_tasks()
+            view_tasks(tasks)
     elif choice == "2":
-            add_task()
+            add_task(tasks)
     elif choice == "3":
-            complete_task()
+            complete_task(tasks)
     elif choice == "4":
-            remove_task()
+            remove_task(tasks)
     elif choice == "5":
         leave_planner()
     else:
         print('You have not entered a valid choice. Please enter a number between 1-5.')
     
-def view_tasks():
+def view_tasks(tasks):
+    print(f"{tasks}")
     print('You have not yet added any tasks')
 
-def add_task():
-    input('Please enter a task: ')
+def add_task(tasks):
+    new_task = input('Please enter a task: ')
+    tasks.append(new_task)
 
 def complete_task():
     print('Select task to list as complete')
@@ -41,11 +43,13 @@ def leave_planner():
     print('Would you like to exit the planner?')
     input('Please enter "yes" or "no" ')
 
+tasks = []
+
 def main():
     show_menu()
     menu_choice()
-    view_tasks()
-    # add_task()
+    # view_tasks()
+    # add_task(tasks)
     # complete_task()
     # remove_task()
     # leave_planner()
